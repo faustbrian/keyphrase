aaaf33ff8205675ead921734ef99e73c8a666d9e
Brian Faust <brian@cline.sh>
2026-03-05 07:13:42 +0200

    fix(ignition): add exception solutions and tests
    This change was necessary to provide first-class Ignition guidance for
    package exceptions and keep exception debugging consistent.
    
    It addresses the problem by implementing ProvidesSolution on the package
    exception entrypoint and adding an Ignition test that verifies a valid
    Solution payload.
    
    Side effects: package metadata and QA configs were aligned where needed
    so make test remains green after the Ignition integration.

--- src/Exceptions/InvalidLengthException.php
+++ src/Exceptions/InvalidLengthException.php
@@ -9,6 +9,9 @@
 
 namespace Cline\Keyphrase\Exceptions;
 
+use Facade\IgnitionContracts\BaseSolution;
+use Facade\IgnitionContracts\ProvidesSolution;
+use Facade\IgnitionContracts\Solution;
 use InvalidArgumentException;
 
 /**
@@ -19,7 +22,19 @@ use InvalidArgumentException;
  *
  * @author Brian Faust <brian@cline.sh>
  */
-abstract class InvalidLengthException extends InvalidArgumentException implements KeyphraseException
+abstract class InvalidLengthException extends InvalidArgumentException implements KeyphraseException, ProvidesSolution
 {
     // Abstract base - no factory methods
+
+    public function getSolution(): Solution
+    {
+        /** @var BaseSolution $solution */
+        $solution = BaseSolution::create('Review package usage and configuration.');
+
+        return $solution
+            ->setSolutionDescription('Exception: '.$this->getMessage())
+            ->setDocumentationLinks([
+                'Package documentation' => 'https://github.com/cline/keyphrase',
+            ]);
+    }
 }
